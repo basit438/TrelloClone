4396a94e0f8d2455d9576031a2ba89ef09a39a40
Basit <basitmanzoor438@gmail.com>
2024-10-19 14:34:18 +0530

    added drag and drop feature using react DND library

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,74 @@
-import { useReducer, useState , createContext } from 'react'
+import { useReducer, useState, createContext } from "react";
 
-import './App.css'
-import CardList from './components/CardList/CardList'
+import "./App.css";
+import CardList from "./components/CardList/CardList";
 
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 // our context provider
 
 export const TrelloContext = createContext();
 
 function App() {
-
   // our masterData/State
 
-  const initState ={
+  const initState = {
     todo: [],
     inProgress: [],
-    done: []
-  }
+    done: [],
+  };
 
-// our reducer function
+  // our reducer function
 
-const reducerfn =(state , action)=>{
-
-  // using switch case because it is faster than if else
-
-  switch (action.type) {
-    case "add-item":
-      console.log(action.payload);
-      return {
-        ...state,
-        [action.payload.listName] : [...state[action.payload.listName] , action.payload.data] // made a copy of the array and pushed the data to it 
+  const reducerfn = (state, action) => {
+    switch (action.type) {
+      case 'add-item':
+        return {
+          ...state,
+          [action.payload.listName]: [
+            ...state[action.payload.listName],
+            action.payload.data,
+          ],
+        };
+  
+      case 'move-item': {
+        // Remove the card from the source list and add it to the target list
+        const fromList = state[action.payload.fromList].filter(item => item !== action.payload.data);
+        const toList = [...state[action.payload.toList], action.payload.data];
+  
+        return {
+          ...state,
+          [action.payload.fromList]: fromList, // Updated source list
+          [action.payload.toList]: toList,     // Updated target list
+        };
       }
+  
+      default:
+        return state;
+    }
+  };
+  
 
-    case "edit-item":
-
-    case "delete-item":
-
-    case "move-item":
-
-
-    default:
-      return state
-  }
-}
+  
 
   // Our reducer hook
 
-  const [state , dispatch] = useReducer(reducerfn, initState)
+  const [state, dispatch] = useReducer(reducerfn, initState);
   return (
-
-    <TrelloContext.Provider value={{state,dispatch}}>
-    <h1 style={{textAlign:"center"}}>Trello Clone</h1>
-   <div style={{display:"flex"}}>
-   <CardList id= {"todo"} title ={"to do"}/>
-    <CardList id ={"inProgress"} title ={"In progress"}/>
-    <CardList id = {"done"} title ={"Done"}/>
-   </div>
-    </TrelloContext.Provider>
-    
-  )
+    <>
+      <h1 style={{ textAlign: "center" }}>Trello Clone</h1>
+      <TrelloContext.Provider value={{ state, dispatch }}>
+        <DndProvider backend={HTML5Backend}>
+          <div style={{ display: "flex" }}>
+            <CardList id={"todo"} title={"to do"} />
+            <CardList id={"inProgress"} title={"In progress"} />
+            <CardList id={"done"} title={"Done"} />
+          </div>
+        </DndProvider>
+      </TrelloContext.Provider>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,54 +1,75 @@
-import React, { useRef } from 'react'
-import Card from '../Card/Card'
-import './CardList.css'
-import { useState ,  useContext} from 'react'
-import { TrelloContext } from '../../App'
-
+import React, { useRef, useState, useContext } from 'react';
+import Card from '../Card/Card';
+import './CardList.css';
+import { TrelloContext } from '../../App';
+import { useDrop } from 'react-dnd';
 
 function CardList(props) {
+  // Using drop from react-dnd to handle dropping of items
+  const [{ isOver }, dropRef] = useDrop({
+    accept: 'CARD', // Accepting only 'CARD' items
+    drop: (item) => {
+      // When a card is dropped, dispatch an action to move the item to the target list
+      ctx.dispatch({
+        type: 'move-item',
+        payload: {
+          fromList: item.id, // The list where the card is coming from
+          toList: props.id,  // The list where the card is being dropped
+          data: item.title,  // The data being transferred
+        },
+      });
+    },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(), // Boolean to indicate if the item is over the list
+    }),
+  });
 
-  const ctx = useContext(TrelloContext);
-  // console.log(ctx);
+  const ctx = useContext(TrelloContext); // Accessing global state and dispatch from context
 
-  const [showAddCard, setShowAddCard] = useState(false)
+  const [showAddCard, setShowAddCard] = useState(false); // State for toggling the add card form
+
   const onAddCardClick = () => {
     setShowAddCard(true);
-  }
+  };
 
-  const onAddClick = () => { 
-    const task = textBoxRef.current.value;
-    // console.log(task);
-    ctx.dispatch(
-      {type: "add-item",
+  const onAddClick = () => {
+    const task = textBoxRef.current.value; // Get the task from the input
+    ctx.dispatch({
+      type: 'add-item',
       payload: {
-        data : task,
-        listName : props.id
-      }
-    })
-  }
+        data: task,        // Data to be added (task)
+        listName: props.id // To which list it should be added (todo, inProgress, done)
+      },
+    });
+    textBoxRef.current.value = ''; // Clear the input after adding the card
+    setShowAddCard(false); // Hide the add card form
+  };
 
-  const textBoxRef = useRef(null);
+  const textBoxRef = useRef(null); // Reference for the input textarea
+
   return (
-    <div className="CardList">
+    <div ref={dropRef} className="CardList" style={{ backgroundColor: isOver ? '#f0f0f0' : 'white' }}>
       <h3>{props.title}</h3>
-      {
-        ctx.state[props.id].map(data => <Card title={data} key={data}/>)
-      }
       
-      {
-        showAddCard ?(
-          <div className="add-card-toggle-container">
-            <textarea ref={textBoxRef} className="add-card-toggle-input-textarea" name="" id=""></textarea>
-            <button onClick={onAddClick}>add</button>
-            <button onClick={() => setShowAddCard(false)}>cancel</button>
-          </div>
-        ):(
-          <span className="add-another-card-btn" onClick={onAddCardClick}>+ Add another card</span>
-        )
-      }
-      
+      {/* Render the cards in this list */}
+      {ctx.state[props.id].map((data) => (
+        <Card id={props.id} title={data} key={data} />
+      ))}
+
+      {/* Add card form toggle */}
+      {showAddCard ? (
+        <div className="add-card-toggle-container">
+          <textarea ref={textBoxRef} className="add-card-toggle-input-textarea"></textarea>
+          <button onClick={onAddClick}>Add</button>
+          <button onClick={() => setShowAddCard(false)}>Cancel</button>
+        </div>
+      ) : (
+        <span className="add-another-card-btn" onClick={onAddCardClick}>
+          + Add another card
+        </span>
+      )}
     </div>
-  )
+  );
 }
 
-export default CardList
+export default CardList;
